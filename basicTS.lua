c1dc765d65de30a9e1eb0117623e6eec53fa8c83
DanielR1 <activatingredstone@gmail.com>
2025-01-29 10:24:38 -0800

    test4

--- basicTS.lua
+++ basicTS.lua
@@ -1,30 +1,25 @@
 
 function update()
-   gcs:send_text(0, "hello, world") -- send the traditional message
-   print("hello world print")
+   --gcs:send_text(0, "hello, world") -- send the traditional message
+   --print("hello world print")
    local current_pos = ahrs:get_position() -- fetch the current position of the vehicle
    local home = ahrs:get_home()            -- fetch the home position of the vehicle
-   if current_pos and home then            -- check that both a vehicle location, and home location are available
-      local distance = current_pos:get_distance(home) -- calculate the distance from home in meters
-      if distance > 1000 then -- if more then 1000 meters away
-      distance = 1000;      -- clamp the distance to 1000 meters
-      end
      -- servo.set_output_pwm(96, 1000 + distance) -- set the servo assigned function 96 (scripting3) to a proportional value
-   end
    local pN = current_pos:lng()
    local pE = current_pos:lat()
-   local distance_to_box_center = 1.25*math.pi*50*pN;
+   local distance_to_box_center = 1.25*3.14*50*pN;
    local alt_ft = current_pos:alt() --/30.48; --convert cm to ft
    local dist_to_ground = alt_ft/math.tan(-10*3.14/180);
    local velNED = ahrs:get_velocity_NED()
-   gcs:send_text(0, alt_ft)
-   gcs:send_text(0, pN)
-   gcs:send_text(0, pE)
-   gcs:send_text(0, velNED:x())
-   gcs:send_text(0, velNED:y())
-   gcs:send_text(0, velNED:z())
-   print(alt_ft)
-   print("printing alt")
+   --gcs:send_text(0, alt_ft)
+  -- gcs:send_text(0, pN)
+  -- gcs:send_text(0, pE)
+  -- gcs:send_text(0, velNED:x())
+  -- gcs:send_text(0, velNED:y())
+   --gcs:send_text(0, velNED:z())
+   gcs:send_text(0, string.format("alt:%.1f pN:%.1f vx:%.1f", alt_ft, pN, velNED:x()))
+   --print(alt_ft)
+   --print("printing alt")
      
    return update(), 1000 -- request "update" to be rerun again 1000 milliseconds (1 second) from now
 end
